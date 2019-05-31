3144db3309518d8a3958b1747c3d7cfe3956c287
Valeria-Esquivel <50186808+Valeria-Esquivel@users.noreply.github.com>
2019-05-30 21:04:52 -0500

    Update conexion.php

--- Practica6/models/conexion.php
+++ Practica6/models/conexion.php
@@ -5,7 +5,7 @@ class Conexion{
 	public function conectar(){
 		//conexion con la base de datos
 		$Otp = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'");
-		$link = new PDO("mysql:host=localhost;dbname=hotel;port=3307","root","usbw", $Otp);
+		$link = new PDO("mysql:host=localhost;dbname=hotel;port=3306","admin","d58003b488046802fee2e1f4a0e677c154a2b621ba0725da", $Otp);
 		
 		return $link;
 
