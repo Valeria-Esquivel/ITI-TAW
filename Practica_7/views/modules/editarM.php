a00efb7ea967ed2ae8b2f6513a14f46370c8b56c
Valeria-Esquivel <50186808+Valeria-Esquivel@users.noreply.github.com>
2019-06-03 23:44:18 -0500

    Create editarM.php

---
+++ Practica_7/views/modules/editarM.php
@@ -0,0 +1,17 @@
+<? php
+session_start ();
+? >
+
+< h1 > EDITAR MAESTRO </ h1 >
+
+< form  method = " post " >
+	
+	<? php
+	// utiliza el metodo de la clase MvcController de controller / controller.php
+		$ editarC  =  new  MvcController ();
+		
+		$ editarC  -> editarMaestrosController ();
+		$ editarC  -> actualizarMaestrosController ();
+	? >
+
+</ form > 
