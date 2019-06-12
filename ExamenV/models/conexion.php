1b1133f9f874fff65af8171e34320daae9db5ee3
Valeria-Esquivel <50186808+Valeria-Esquivel@users.noreply.github.com>
2019-06-12 16:11:06 -0500

    Update conexion.php

--- ExamenV/models/conexion.php
+++ ExamenV/models/conexion.php
@@ -6,7 +6,7 @@ class Conexion{
 	
 		
 		
-		$link = new PDO("mysql:host=localhost;dbname=catalogo","root","");
+		$link = new PDO("mysql:host=localhost;dbname=catalogo","admin","d58003b488046802fee2e1f4a0e677c154a2b621ba0725da");
 		
 		
 		
